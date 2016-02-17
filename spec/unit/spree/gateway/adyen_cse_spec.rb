9c234f3aab9b962d48d557458778278edf636ecb
Seb Ashton <seb@sebashton.com>
2016-02-17 10:06:47 +0000

    Wrap api calls in a context block

--- spec/unit/spree/gateway/adyen_cse_spec.rb
+++ spec/unit/spree/gateway/adyen_cse_spec.rb
@@ -18,66 +18,71 @@ describe Spree::Gateway::AdyenCse do
     double('Response', response_attr)
   end
 
-  before do
-    expect(gateway.provider).to receive(method).and_return(response)
-  end
+  it { expect(subject.method_type).to eq('adyen_cse') }
+  it { expect(subject.actions).to match_array(%w(credit void)) }
 
-  describe '#capture' do
-    let(:method) { :capture_payment }
-
-    subject { gateway.capture(10, psp_reference, currency: currency).authorization }
-
-    include_examples 'a successful adyen response'
-  end
-
-  describe '#credit' do
-    let(:method) { :cancel_or_refund_payment }
-
-    subject { gateway.credit(10, credit_card, psp_reference, currency: currency).authorization }
-
-    include_examples 'a successful adyen response'
-  end
-
-  describe '#void' do
-    let(:method) { :cancel_payment }
-
-    subject { gateway.void(psp_reference, credit_card).authorization }
-
-    include_examples 'a successful adyen response'
-  end
-
-  context 'when the provider returns an error' do
-    let(:success) { false }
-
-    let(:additional_response_attr) do
-      { result_code: 'T35T', refusal_reason: 'Only testing' }
+  context 'provider API calls' do
+    before do
+      expect(gateway.provider).to receive(method).and_return(response)
     end
 
     describe '#capture' do
       let(:method) { :capture_payment }
-      let(:expected_reponse_string) { 'T35T - Only testing' }
 
-      subject { gateway.capture(10, psp_reference, currency: currency) }
+      subject { gateway.capture(10, psp_reference, currency: currency).authorization }
 
-      include_examples 'a failed adyen response'
+      include_examples 'a successful adyen response'
     end
 
     describe '#credit' do
       let(:method) { :cancel_or_refund_payment }
-      let(:expected_reponse_string) { 'Only testing' }
 
-      subject { gateway.credit(10, credit_card, psp_reference, currency: currency) }
+      subject { gateway.credit(10, credit_card, psp_reference, currency: currency).authorization }
 
-      include_examples 'a failed adyen response'
+      include_examples 'a successful adyen response'
     end
 
     describe '#void' do
       let(:method) { :cancel_payment }
-      let(:expected_reponse_string) { 'T35T - Only testing' }
 
-      subject { gateway.void(psp_reference, credit_card) }
+      subject { gateway.void(psp_reference, credit_card).authorization }
 
-      include_examples 'a failed adyen response'
+      include_examples 'a successful adyen response'
+    end
+
+    context 'when it returns an error' do
+      let(:success) { false }
+
+      let(:additional_response_attr) do
+        { result_code: 'T35T', refusal_reason: 'Only testing' }
+      end
+
+      describe '#capture' do
+        let(:method) { :capture_payment }
+        let(:expected_reponse_string) { 'T35T - Only testing' }
+
+        subject { gateway.capture(10, psp_reference, currency: currency) }
+
+        include_examples 'a failed adyen response'
+      end
+
+      describe '#credit' do
+        let(:method) { :cancel_or_refund_payment }
+        let(:expected_reponse_string) { 'Only testing' }
+
+        subject { gateway.credit(10, credit_card, psp_reference, currency: currency) }
+
+        include_examples 'a failed adyen response'
+      end
+
+      describe '#void' do
+        let(:method) { :cancel_payment }
+        let(:expected_reponse_string) { 'T35T - Only testing' }
+
+        subject { gateway.void(psp_reference, credit_card) }
+
+        include_examples 'a failed adyen response'
+      end
     end
   end
 end
